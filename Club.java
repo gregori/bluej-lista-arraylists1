a585ccf6b90269221587db333e56bebdef430676
Rodrigo Gregori <rodrigo.gregori@catolicasc.org.br>
2017-09-08 20:41:33 -0300

    Compartilhamento inicial do projeto

---
+++ Club.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+/**
+ * Store details of club memberships.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Club
+{
+    // Define any necessary fields here ...
+    private ArrayList<Membership> members;
+    
+    /**
+     * Constructor for objects of class Club
+     */
+    public Club()
+    {
+        // Initialise any fields here ...
+        members = new ArrayList<>();
+    }
+
+    /**
+     * Add a new member to the club's list of members.
+     * @param member The member object to be added.
+     */
+    public void join(Membership member)
+    {
+        members.add(member);
+    }
+
+    /**
+     * @return The number of members (Membership objects) in
+     *         the club.
+     */
+    public int numberOfMembers()
+    {
+        return members.size();
+    }
+}
